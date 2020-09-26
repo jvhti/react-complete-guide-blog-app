f69f27ead684f904966a14fda2b6d3d7ed5826be
jvhti <jvhti@hotmail.com>
2020-09-26 10:40:20 -0300

    Get data from the server with Axios

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,13 +7,22 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+  state = {
+    posts: []
+  }
+
+  componentDidMount() {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+        .then((response) => {
+          this.setState({posts: response.data});
+        });
+  }
+
   render() {
     return (
         <div>
           <section className="Posts">
-            <Post/>
-            <Post/>
-            <Post/>
+            {this.state.posts.map((p) => <Post key={p.id} title={p.title}/>)}
           </section>
           <section>
             <FullPost/>
